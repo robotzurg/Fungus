355190a5d24fd1df8e397e3c5519970c95bb53e4
Jeffdev <robotzurg@gmail.com>
2021-10-27 02:59:53 -0600

    Initial commit

---
+++ fungus-bot/commands/brody.js
@@ -0,0 +1,25 @@
+const { SlashCommandBuilder } = require('@discordjs/builders');
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('brody')
+		.setDescription('Bully Brody!')
+		.addBooleanOption(option => 
+            option.setName('ping')
+                .setDescription('Ping?')
+                .setRequired(false)),
+	async execute(interaction) {
+		const ping = interaction.options.getBoolean('ping');
+        const outputs = ['Brody is just like Quackity', 'Brody is a bitch', 'Brody looks like he can\'t swim',
+                         'Brody likes it up the ass', 'Brody would fuck a pie like in the Hit Classic, American Pie', 
+						 'Brody would experience sexual attraction to the easter bunny', 'Brody isnt an ass guy, or a tits guy, or a thigh guy. He is a furry guy.',
+						 'Brody owes the person who pinged this 20 dollars', 'Brody watches anime porn', 'Brody is a giggler',
+						 'Brody would trade his girlfriend for Ryan Reynolds. This isnt a bullying thing, its just true.', 
+						 'Brody got an anus tattooed between his butt cheeks because he was born without one.', 'Brody is not Technoblade',
+						 'Brody would stick a cactus up his ass if he was horny enough'];
+
+        let random = outputs[Math.floor(Math.random() * outputs.length)];
+		if (ping === true) random = random.replace('Brody', '<@450496000289669122>');
+		interaction.editReply(random);
+	},
+};
---
+++ fungus-bot/index.js
@@ -0,0 +1,86 @@
+// require the discord.js module
+const fs = require('fs');
+const Discord = require('discord.js');
+const { token } = require('./config.json');
+const { REST } = require('@discordjs/rest');
+const { Routes } = require('discord-api-types/v9');
+
+// create a new Discord client and give it some variables
+const { Client, Intents } = require('discord.js');
+const myIntents = new Intents();
+myIntents.add('GUILD_PRESENCES', 'GUILD_MEMBERS', 'GUILD_PRESENCES');
+
+const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILD_MESSAGE_REACTIONS, 
+                            Intents.FLAGS.GUILD_MEMBERS, Intents.FLAGS.GUILD_PRESENCES], partials: ['MESSAGE', 'CHANNEL', 'REACTION'] });
+client.commands = new Discord.Collection();
+const registerCommands = [];
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+
+// Place your client and guild ids here
+const clientId = '902832747368833025';
+const guildId = '845890540486787084';
+
+for (const file of commandFiles) {
+	const command = require(`./commands/${file}`);
+    if (command.type === undefined) {
+        // Slash Commands
+        client.commands.set(command.data.name, command);
+        registerCommands.push(command.data.toJSON());
+    } else {
+        // Context Menu Commands (these have a different structure)
+        client.commands.set(command.name, command);
+        registerCommands.push(command);
+    }
+}
+
+const rest = new REST({ version: '9' }).setToken(token);
+
+(async () => {
+	try {
+		console.log('Started refreshing application (/) commands.');
+
+		await rest.put(
+			Routes.applicationGuildCommands(clientId, guildId),
+			{ body: registerCommands },
+		);
+
+		console.log('Successfully reloaded application (/) commands.');
+	} catch (error) {
+		console.error(error);
+	}
+})();
+
+client.once('ready', async () => {
+    console.log('Ready!');
+    const date = new Date().toLocaleTimeString().replace("/.*(d{2}:d{2}:d{2}).*/", "$1");
+    console.log(date);
+});
+
+// Listen for interactions (INTERACTION COMMAND HANDLER)
+client.on('interactionCreate', async interaction => {
+
+	if (!interaction.isCommand() && !interaction.isContextMenu()) return;
+
+	await interaction.deferReply();
+
+    const command = client.commands.get(interaction.commandName);
+
+    if (!command) return;
+
+    try {
+        await command.execute(interaction, client);
+    } catch (error) {
+        await console.error(error);
+        await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+    }
+    
+});
+
+
+// Listen for messages
+client.on('messageCreate', async message => {
+
+});
+
+// login to Discord
+client.login(token);
